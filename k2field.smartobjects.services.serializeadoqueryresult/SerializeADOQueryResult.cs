cca34f303d566927c6f26edfd98671b6a66f9ce7
jonnoking <jonno@jonnoking.com>
2014-09-24 17:22:25 -0700

    0.0.0.1

---
+++ k2field.smartobjects.services.serializeadoqueryresult/SerializeADOQueryResult.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+using SourceCode.SmartObjects.Services.ServiceSDK.Objects;
+using Attributes = SourceCode.SmartObjects.Services.ServiceSDK.Attributes;
+using SourceCode.SmartObjects.Services.ServiceSDK.Types;
+
+using SourceCode.Data.SmartObjectsClient;
+using SourceCode.Hosting.Client.BaseAPI;
+using SourceCode.SmartObjects.Client;
+
+namespace K2Field.SmartObjects.Services.SerializeADOQueryResult
+{
+    [Attributes.ServiceObject("SerializeADOQueryResult", "Serialize ADO Query Result", "Serialize ADO Query Result")]
+    public class SerializeADOQueryResult
+    {
+        private string adoquery = "";
+        [Attributes.Property("ADOQuery", SoType.Memo, "ADO Query", "ADO Query")]
+        public string ADOQuery
+        {
+            get { return adoquery; }
+            set { adoquery = value; }
+        }
+
+        private string jsonresult = "";
+        [Attributes.Property("JSONResult", SoType.Memo, "JSON Result", "JSON Result")]
+        public string JSONResult
+        {
+            get { return jsonresult; }
+            set { jsonresult = value; }
+        }
+
+        private string xmlresult = "";
+        [Attributes.Property("XMLResult", SoType.Memo, "XML Result", "XML Result")]
+        public string XMLResult
+        {
+            get { return xmlresult; }
+            set { xmlresult = value; }
+        }
+
+
+        [Attributes.Method("ADOQuerytoJSON", SourceCode.SmartObjects.Services.ServiceSDK.Types.MethodType.Read, "ADO Query to JSON", "ADO Query to JSON",
+        new string[] { "ADOQuery" }, //required property array (no required properties for this sample)
+        new string[] { "ADOQuery" }, //input property array (no optional input properties for this sample)
+        new string[] { "ADOQuery", "JSONResult" })] //return property array (2 properties for this example)
+        public SerializeADOQueryResult ADOQuerytoJSONExecute()
+        {
+            DataTable results = QuerySmartObject();
+
+            StringBuilder sb = new StringBuilder();
+
+            int rowCount = 0;
+
+            if (results.Rows.Count > 1)
+            {
+                sb.Append("[");
+                sb.Append("\n");
+            }
+
+            foreach (DataRow r in results.Rows)
+            {
+                rowCount++;
+
+                sb.Append("{");
+                sb.Append("\n");
+                int colCount = 0;
+                foreach (DataColumn c in r.Table.Columns)
+                {
+                    colCount++;
+                    sb.Append("\"");
+                    sb.Append(c.ColumnName);
+                    sb.Append("\"");
+                    sb.Append(": ");
+                    sb.Append("\"");
+
+                    // all values treated as strings
+                    string val = string.Empty;
+                    if (!r.IsNull(c))
+                    {
+                        val = r[c].ToString();
+                        val = val.Replace("\r\n", " ").Replace("\n", " ").Replace("\r", " ");
+
+                    }
+                    sb.Append(val);
+                    sb.Append("\"");
+                    if (colCount < r.Table.Columns.Count)
+                    {
+                        sb.Append(",");
+                        sb.Append("\n");
+                    }
+                }
+                sb.Append("\n");
+                sb.Append("}");
+
+                if (rowCount < results.Rows.Count)
+                {
+                    sb.Append(",");
+                    sb.Append("\n");
+                }
+
+            }
+            if (results.Rows.Count > 1)
+            {
+                sb.Append("\n");
+                sb.Append("]");
+            }
+
+            this.JSONResult = sb.ToString();
+
+
+            return this;
+        }
+
+        [Attributes.Method("ADOQuerytoXML", SourceCode.SmartObjects.Services.ServiceSDK.Types.MethodType.Read, "ADO Query to XML", "ADO Query to XML",
+        new string[] { "ADOQuery" }, //required property array (no required properties for this sample)
+        new string[] { "ADOQuery" }, //input property array (no optional input properties for this sample)
+        new string[] { "ADOQuery", "XMLResult" })] //return property array (2 properties for this example)
+        public SerializeADOQueryResult ADOQuerytoXMLExecute()
+        {
+
+            DataTable results = QuerySmartObject();
+
+            StringBuilder sb = new StringBuilder();
+
+            int rowCount = 0;
+            sb.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            sb.Append("\n");
+            if (results.Rows.Count > 1)
+            {
+                sb.Append("<results>");
+                sb.Append("\n");
+            }
+
+            foreach (DataRow r in results.Rows)
+            {
+                rowCount++;
+
+                sb.Append("<row>");
+                sb.Append("\n");
+                int colCount = 0;
+                foreach (DataColumn c in r.Table.Columns)
+                {
+                    colCount++;
+                    sb.Append("<");
+                    sb.Append(c.ColumnName);
+                    sb.Append(">");
+                    // not parsing special characters or adding cdata sections
+                    // stripping line breaks
+                    string val = string.Empty;
+                    if (!r.IsNull(c))
+                    {
+                        val = r[c].ToString();
+                        val = val.Replace("\r\n", " ").Replace("\n", " ").Replace("\r", " ");
+
+                    }
+                    sb.Append(val);
+                    sb.Append("</");
+                    sb.Append(c.ColumnName);
+                    sb.Append(">");
+                    if (colCount < r.Table.Columns.Count)
+                    {
+                        sb.Append("\n");
+                    }
+                }
+                sb.Append("\n");
+                sb.Append("</row>");
+
+                if (rowCount < results.Rows.Count)
+                {
+                    sb.Append("\n");
+                }
+
+            }
+            if (results.Rows.Count > 1)
+            {
+                sb.Append("\n");
+                sb.Append("</results>");
+            }
+
+            this.XMLResult = sb.ToString();
+
+
+            return this;
+        }
+
+
+        private DataTable QuerySmartObject()
+        {
+            DataTable results = new DataTable();
+
+            string sql_query = adoquery;
+
+            //SourceCode.SmartObjects.Client.SmartObjectClientServer server = new SmartObjectClientServer();
+            SCConnectionStringBuilder SmOBuilder = new SCConnectionStringBuilder();
+            SmOBuilder.Host = "localhost";
+            SmOBuilder.Port = 5555;
+            SmOBuilder.Integrated = true;
+            SmOBuilder.IsPrimaryLogin = true;
+
+            using (SOConnection connection = new SOConnection(SmOBuilder.ToString()))
+            using (SOCommand command = new SOCommand(sql_query, connection))
+            using (SODataAdapter adapter = new SODataAdapter(command))
+            {
+                connection.DirectExecution = true;
+                adapter.Fill(results);
+            }
+
+            return results;
+        }
+
+    }
+}
---
+++ k2field.smartobjects.services.splunk/SplunkMessage.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+using SourceCode.SmartObjects.Services.ServiceSDK.Objects;
+using Attributes = SourceCode.SmartObjects.Services.ServiceSDK.Attributes;
+using SourceCode.SmartObjects.Services.ServiceSDK.Types;
+using Splunk;
+using SplunkSDKHelper;
+
+
+namespace K2Field.SmartObjects.Services.SplunkService
+{
+    [Attributes.ServiceObject("Splunk", "Splunk", "Splunk")]
+    public class SplunkMessage
+    {
+        private string index = "";
+        [Attributes.Property("Index", SoType.Text, "Index", "Index")]
+        public string Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+
+        private string source = "";
+        [Attributes.Property("Source", SoType.Text, "Source", "Source")]
+        public string Source
+        {
+            get { return source; }
+            set { source = value; }
+        }
+
+        private string sourcetype = "";
+        [Attributes.Property("SourceType", SoType.Text, "SourceType", "SourceType")]
+        public string SourceType
+        {
+            get { return sourcetype; }
+            set { sourcetype = value; }
+        }
+
+        private string message = "";
+        [Attributes.Property("Message", SoType.Memo, "Message", "Message")]
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+
+        private string resultstatus = "";
+        [Attributes.Property("ResultStatus", SoType.Text, "ResultStatus", "ResultStatus")]
+        public string ResultStatus
+        {
+            get { return resultstatus; }
+            set { resultstatus = value; }
+        }
+
+        private string resultmessage = "";
+        [Attributes.Property("ResultMessage", SoType.Text, "ResultMessage", "ResultMessage")]
+        public string ResultMessage
+        {
+            get { return resultmessage; }
+            set { resultmessage = value; }
+        }
+
+
+        [Attributes.Method("SubmitMessage", SourceCode.SmartObjects.Services.ServiceSDK.Types.MethodType.Execute, "SubmitMessage", "SubmitMessage",
+        new string[] { "Message", "Index", "Source", "SourceType" }, //required property array (no required properties for this sample)
+        new string[] { "Message", "Index", "Source", "SourceType" }, //input property array (no optional input properties for this sample)
+        new string[] { "Message", "Index", "Source", "SourceType", "ResultStatus", "ResultMessage" })] //return property array (2 properties for this example)
+        public SplunkMessage PostMessageToSplunk()
+        {
+            try
+            {
+                // Load connection info for Splunk server in .splunkrc file.
+                var cli = Command.Splunk("submit");
+                //cli.Parse(argv);
+
+                string[] cargs = { };
+                cli.Parse(cargs);
+
+
+                var service = Splunk.Service.Connect(cli.Opts);
+
+                var args = new ReceiverSubmitArgs
+                {
+                    Index = this.Index,
+                    Source = this.Source,
+                    SourceType = this.SourceType
+                };
+
+                var receiver = new Receiver(service);
+
+                receiver.Submit(args, message);
+
+                this.ResultStatus = "OK";
+            }
+            catch(Exception ex)
+            {
+                this.ResultStatus = "Error";
+                this.ResultMessage = ex.Message;
+            }
+
+
+//            # Splunk host (default: localhost)
+//host=localhost
+//# Splunk admin port (default: 8089)
+//port=8089
+//# Splunk username
+//username=admin
+//# Splunk password
+//password=K2pass!
+//# Access scheme (default: https)
+//scheme=https
+
+//.splunkrc
+
+            return this;
+        }
+
+    }
+}
